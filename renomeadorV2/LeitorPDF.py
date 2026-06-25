e7797212e22e91626668e3dd7ee0db8f638c472a
caioosantss <gilkamesh8@gmail.com>
2026-06-24 22:55:16 -0300

    update

--- renomeadorV2/LeitorPDF.py
+++ renomeadorV2/LeitorPDF.py
@@ -1,20 +1,24 @@
 import pymupdf
 from tkinter import Tk, filedialog
-import os
 
+
+caminho_pasta = {}
 def extrair_texto():
     root = Tk()
     root.withdraw()
+    dados = []
 
     pasta = filedialog.askdirectory(
     title="Selecione a pasta",
 )
-
+    caminho_pasta["pasta"] = pasta
     for PDF in os.listdir(pasta):
-        caminho = os.path.join(pasta, PDF)
-        arquivo = pymupdf.open(caminho) 
+        if PDF.lower().endswith('.pdf'):
+            caminho = os.path.join(pasta, PDF)
+            arquivo = pymupdf.open(caminho) 
         
         for pagina in arquivo:
             texto = pagina.get_text()
-            print(texto)
+            dados.append(texto)
+    return dados
         
---
+++ renomeadorV2/renomeador.py
@@ -0,0 +1,13 @@
+import re
+import os
+from LeitorPDF import extrair_texto
+from LeitorPDF import caminho_pasta
+import banco
+
+
+def renomear():
+
+    codigo = re.compile(r'\b(?!3)\d{8}(?=\s)')
+    arquivos = extrair_texto()
+    ativos = banco.data.verificar_ativos()
+    print(ativos)
