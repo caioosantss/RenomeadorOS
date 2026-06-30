6f44c0623debb8f7050c088cf67b414d481552a6
caioosants <gilkamesh8@gmail.com>
2026-06-30 15:04:10 -0300

    update

--- renomeadorV2/renomeador.py
+++ renomeadorV2/renomeador.py
@@ -5,10 +5,20 @@ import banco
 
 
 def renomear():
-
-    codigo = re.compile(r'\b(?!3)\d{8}(?=\s)')
+   
     arquivos = LeitorPDF.extrair_texto()
-    ativos = banco.data.verificar_ativos()
-
-
-print(renomear())
+    
+    if arquivos:
+        codigo = re.compile(r'\b(?!3)\d{8}(?=\s)')
+        ativosNT = "|".join(banco.data.verificar_ativos())
+        ativos = re.compile(rf"\b({ativosNT})\-?[A-Z0-9]*\d[A-Z0-9]*\b")
+        
+        
+    for contador in range(1, len(arquivos) + 1):
+        codigoPDF = re.search(codigo, arquivos[f"PDF{contador}"])
+        
+        if codigoPDF:
+            ativoPDF = re.search(ativos, arquivos[f'PDF{contador}'])
+            return print(ativoPDF.group())
+                    
+renomear()
