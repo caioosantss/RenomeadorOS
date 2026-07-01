e12e0ceb25c7dc298d87d66b8ddaf5ba113143a9
caioosantss <gilkamesh8@gmail.com>
2026-06-30 22:05:44 -0300

    update

--- renomeadorV2/renomeador.py
+++ renomeadorV2/renomeador.py
@@ -3,22 +3,32 @@ import os
 import LeitorPDF
 import banco
 
-
 def renomear():
    
     arquivos = LeitorPDF.extrair_texto()
     
     if arquivos:
-        codigo = re.compile(r'\b(?!3)\d{8}(?=\s)')
+        codigo = re.compile(r'\b\d{8}(?=\s)')
         ativosNT = "|".join(banco.data.verificar_ativos())
         ativos = re.compile(rf"\b({ativosNT})\-?[A-Z0-9]*\d[A-Z0-9]*\b")
         
         
-    for contador in range(1, len(arquivos) + 1):
-        codigoPDF = re.search(codigo, arquivos[f"PDF{contador}"])
+        for antigo_nome in arquivos:
+            codigoPDF = re.search(codigo, arquivos[antigo_nome])
         
-        if codigoPDF:
-            ativoPDF = re.search(ativos, arquivos[f'PDF{contador}'])
-            return print(ativoPDF.group())
-                    
-renomear()
+            if codigoPDF:
+                ativoPDF = re.search(ativos, arquivos[antigo_nome])
+            
+                if ativoPDF:
+                    novo_nome = (f'OS {codigoPDF.group()} {ativoPDF.group()}.pdf')
+                    os.rename(os.path.join(LeitorPDF.caminho_pasta[0], antigo_nome), os.path.join(LeitorPDF.caminho_pasta[0], novo_nome))
+
+                else:
+                    return print(f"não encontrado ativo da OS {codigoPDF.group()}")
+
+            else:
+                return print("não encotrado código")
+
+    else:
+        return print("nenhum texto foi extraído")
+
