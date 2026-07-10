5ed43ecfbca6db622f8cabc8e55e740daee5cce9
caioosants <gilkamesh8@gmail.com>
2026-07-10 14:42:22 -0300

    update

--- renomeadorV3/interface/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Módulo de Interface do Renomeador de OS
-Contém os componentes da interface gráfica
-"""
-
-from .servicos import ServicoInterface
-from .principal import app
-
-__all__ = ['ServicoInterface', 'app']
